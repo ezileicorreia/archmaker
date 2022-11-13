4db2de7b8eb8738c9c4c86542abdd7d2c3f68b4a
Ezilei Correia <ezilei@yahoo.com.br>
2022-11-13 16:26:57 -0300

    Português

---
+++ translations/ArchMaker_pt-BR.ts
@@ -0,0 +1,435 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>AURDialogForm.ui</name>
+    <message>
+        <location filename="../res/qml/AURDialogForm.ui.qml" line="19"/>
+        <source>No search results.</source>
+        <translation>Nenhum resultado de pesquisa.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/AURDialogForm.ui.qml" line="28"/>
+        <source>Select an AUR package</source>
+        <translation>Selecione um pacote AUR</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/AURDialogForm.ui.qml" line="110"/>
+        <source>Add</source>
+        <translation>Adicionar</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/AURDialogForm.ui.qml" line="117"/>
+        <location filename="../res/qml/AURDialogForm.ui.qml" line="126"/>
+        <source>Search</source>
+        <translation>Procurar</translation>
+    </message>
+</context>
+<context>
+    <name>Page1Form.ui</name>
+    <message>
+        <location filename="../res/qml/Page1Form.ui.qml" line="20"/>
+        <source>Welcome to ArchMaker!</source>
+        <translation>Bem-vindo ao ArchMaker!</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page1Form.ui.qml" line="32"/>
+        <source>This assistant will guide you step-by-step through the creation of your custom, arch-based GNU/Linux-distribution.
+
+You just have to complete the following 7 steps:
+1. Programs that need to be installed (normal repositories)
+2. Programs that need to be installed (AUR)
+3. Standard-folder-content for new users (/etc/skel)
+4. lsb-release information
+5. Slideshow for calamares (the installer)
+6. Summary
+7. Saving the final script</source>
+        <translation>Este assistente irá guiá-lo passo a passo através da criação de sua distribuição GNU/Linux personalizada e baseada em Arch.
+
+Você só precisa completar os 7 passos a seguir:
+1. Programas que precisam ser instalados (repositórios normais)
+2. Programas que precisam ser instalados (AUR)
+3. Conteúdo da pasta padrão para novos usuários (/etc/skel)
+4. Informações lsb-release
+5. Apresentação de slides para calamares (o instalador)
+6. Resumo
+7. Salvando o script final</translation>
+    </message>
+</context>
+<context>
+    <name>Page2Form.ui</name>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="378"/>
+        <source>Select currently installed programs</source>
+        <translation>Selecione os programas atualmente instalados</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="388"/>
+        <source>Edit manually</source>
+        <translation>Edite manualmente</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="400"/>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="403"/>
+        <source>Desktop</source>
+        <translation>Desktop</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="406"/>
+        <source>Internet</source>
+        <translation>Internet</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="409"/>
+        <source>Multimedia</source>
+        <translation>Multimídia</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="412"/>
+        <source>Graphics</source>
+        <translation>Gráficos</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="415"/>
+        <source>Accessories</source>
+        <translation>Acessórios</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="418"/>
+        <source>Office</source>
+        <translation>Escritório</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page2Form.ui.qml" line="421"/>
+        <source>Other</source>
+        <translation>Outro</translation>
+    </message>
+</context>
+<context>
+    <name>Page3</name>
+    <message>
+        <location filename="../res/qml/Page3.qml" line="27"/>
+        <source>No search results.</source>
+        <translation>Nenhum resultado da pesquisa.</translation>
+    </message>
+</context>
+<context>
+    <name>Page3Form.ui</name>
+    <message>
+        <location filename="../res/qml/Page3Form.ui.qml" line="33"/>
+        <source>Which programs would you like to install?</source>
+        <translation>Quais programas você gostaria de instalar?</translation>
+    </message>
+</context>
+<context>
+    <name>Page4Form.ui</name>
+    <message>
+        <location filename="../res/qml/Page4Form.ui.qml" line="28"/>
+        <source>/etc/skel-folder</source>
+        <translation>/etc/skel-pasta</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page4Form.ui.qml" line="40"/>
+        <source>The /etc/skel-folder contains files/folders, which will be copied to every new user&apos;s home-folder when creating the account. It can be used to store standard configuration-files.</source>
+        <translation>A pasta /etc/skel-pasta contém arquivos/pastas, que serão copiados para a pasta inicial de cada novo usuário ao criar a conta. Ele pode ser usado para armazenar arquivos de configuração padrão.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page4Form.ui.qml" line="59"/>
+        <source>Keep /etc/skel empty
+            (no personal settings).</source>
+        <translation>Mantenha /etc/skel vazio
+            (sem configurações pessoais).</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page4Form.ui.qml" line="76"/>
+        <source>Use a custom /etc/skel-folder
+            (with personal settings).</source>
+        <translation>Use uma pasta /etc/skel personalizada
+            (com configurações pessoais).</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page4Form.ui.qml" line="91"/>
+        <source>No folder selected.</source>
+        <translation>Nenhuma pasta selecionada.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page4Form.ui.qml" line="102"/>
+        <source>Pick a folder.</source>
+        <translation>Escolha uma Pasta.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page4Form.ui.qml" line="114"/>
+        <source>Use my home-folder
+            (only the configuration files will
+                be copied).</source>
+        <translation>Usar minha pasta home
+            (somente os arquivos de configuração
+                serão copiados).</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page4Form.ui.qml" line="131"/>
+        <source>Please select an /etc/skel-folder</source>
+        <translation>Selecione uma pasta /etc/skel</translation>
+    </message>
+</context>
+<context>
+    <name>Page5Form.ui</name>
+    <message>
+        <location filename="../res/qml/Page5Form.ui.qml" line="37"/>
+        <source>The file &quot;/etc/lsb-release&quot; contains information about the distribution. The selected name will also be used for dual-boot-entries.</source>
+        <translation>O arquivo &quot;/etc/lsb-release&quot; contém informações sobre a distribuição. O nome selecionado também será usado para entradas de inicialização dual-boot.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page5Form.ui.qml" line="55"/>
+        <source>Name of the distribution:</source>
+        <translation>Nome da Distribuição:</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page5Form.ui.qml" line="64"/>
+        <location filename="../res/qml/Page5Form.ui.qml" line="66"/>
+        <source>mydistro</source>
+        <translation>MinhaDistro</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page5Form.ui.qml" line="70"/>
+        <source>Description:</source>
+        <translation>Descrição:</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page5Form.ui.qml" line="79"/>
+        <location filename="../res/qml/Page5Form.ui.qml" line="81"/>
+        <source>The best distro ever!</source>
+        <translation>A melhor distro de todos os tempos!</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page5Form.ui.qml" line="85"/>
+        <source>First version:</source>
+        <translation>Primeira Versão:</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page5Form.ui.qml" line="100"/>
+        <source>First codename:</source>
+        <translation>Primeiro Codinome:</translation>
+    </message>
+</context>
+<context>
+    <name>Page6Form.ui</name>
+    <message>
+        <location filename="../res/qml/Page6Form.ui.qml" line="27"/>
+        <source>Slideshow</source>
+        <translation>Apresentação de Slides</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page6Form.ui.qml" line="39"/>
+        <source>The slideshow will be shown during the installation of the distro. The folder should contain the images as PNG-files with a recommended size of 800x400px.</source>
+        <translation>A apresentação de slides será exibida durante a instalação da distro. A pasta deve conter as imagens como arquivos PNG com um tamanho recomendado de 800x400px.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page6Form.ui.qml" line="58"/>
+        <source>Use the default-slideshow.</source>
+        <translation>Use a apresentação de slides
+padrão.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page6Form.ui.qml" line="74"/>
+        <source>Use a custom slideshow.</source>
+        <translation>Use uma apresentação de slides
+personalizada.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page6Form.ui.qml" line="88"/>
+        <source>No folder selected.</source>
+        <translation>Nenhuma pasta selecionada.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page6Form.ui.qml" line="99"/>
+        <source>Pick a folder.</source>
+        <translation>Escolha uma Pasta.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page6Form.ui.qml" line="114"/>
+        <source>Please select a slideshow-folder</source>
+        <translation>Selecione uma pasta de apresentação de slides</translation>
+    </message>
+</context>
+<context>
+    <name>Page7</name>
+    <message>
+        <location filename="../res/qml/Page7.qml" line="6"/>
+        <source>Packages that need to be installed: selected packages
+Packages from the AUR: 
+</source>
+        <translation>Pacotes que precisam ser instalados: pacotes selecionados
+Pacotes do AUR: 
+</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page7.qml" line="7"/>
+        <source>
+/etc/skel-folder: </source>
+        <translation>
+pasta /etc/skel: </translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page7.qml" line="8"/>
+        <source>
+Name of the distribution: </source>
+        <translation>
+Nome da Distribuição: </translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page7.qml" line="9"/>
+        <source>
+Description of the Distribution: </source>
+        <translation>
+Descrição da Distribuição: </translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page7.qml" line="10"/>
+        <source>
+First version of the Distribution: </source>
+        <translation>
+Primeira Versão da Distribuição: </translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page7.qml" line="11"/>
+        <source>
+First codename of the Distribution: </source>
+        <translation>
+Primeiro Codinome da Distribuição: </translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page7.qml" line="12"/>
+        <source>
+Slideshow-folder: </source>
+        <translation>
+Pasta de apresentação de slides: </translation>
+    </message>
+</context>
+<context>
+    <name>Page7Form.ui</name>
+    <message>
+        <location filename="../res/qml/Page7Form.ui.qml" line="25"/>
+        <source>Summary</source>
+        <translation>Resumo</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/Page7Form.ui.qml" line="51"/>
+        <source>Generate</source>
+        <translation>Gerar</translation>
+    </message>
+</context>
+<context>
+    <name>ScriptDialogForm.ui</name>
+    <message>
+        <location filename="../res/qml/ScriptDialogForm.ui.qml" line="33"/>
+        <source>The script has been generated successfully! :)</source>
+        <translation>O script foi gerado com sucesso! :)</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/ScriptDialogForm.ui.qml" line="46"/>
+        <source>The generation-script for your distribution has been generated successfully. Now you can save it to run it later in a terminal or you can run it now.</source>
+        <translation>O script de geração para sua distribuição foi gerado com sucesso. Agora você pode salvá-lo para executá-lo mais tarde em um terminal ou pode executá-lo agora.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/ScriptDialogForm.ui.qml" line="57"/>
+        <source>Back</source>
+        <translation>Voltar</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/ScriptDialogForm.ui.qml" line="71"/>
+        <source>Save</source>
+        <translation>Salvar</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/ScriptDialogForm.ui.qml" line="81"/>
+        <source>Run</source>
+        <translation>Rodar</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/ScriptDialogForm.ui.qml" line="92"/>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/ScriptDialogForm.ui.qml" line="105"/>
+        <source>Please select a build-folder for your distro</source>
+        <translation>Selecione uma pasta de compilação para sua distro</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../res/qml/main.qml" line="92"/>
+        <location filename="../res/qml/main.qml" line="99"/>
+        <source>Empty /etc/skel-folder</source>
+        <translation>Pasta /etc/skel vazia</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="93"/>
+        <source>Config from ~ as /etc/skel</source>
+        <translation>Configuração a partir de ~ como /etc/skel</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="95"/>
+        <location filename="../res/qml/main.qml" line="114"/>
+        <source>No folder selected.</source>
+        <translation>Nenhuma pasta selecionada.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="97"/>
+        <source>Custom /etc/skel-folder: </source>
+        <translation>Pasta /etc/skel personalizada: </translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="112"/>
+        <location filename="../res/qml/main.qml" line="118"/>
+        <source>Use default slideshow.</source>
+        <translation>Use a apresentação de slides padrão.</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="116"/>
+        <source>Use custom slideshow: </source>
+        <translation>Usar apresentação de slides personalizada: </translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="154"/>
+        <source>Welcome</source>
+        <translation>Bem-Vindo</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="157"/>
+        <source>Programs (repos)</source>
+        <translation>Repos</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="160"/>
+        <source>Programs (AUR)</source>
+        <translation>AUR</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="163"/>
+        <source>/etc/skel-folder</source>
+        <translation>/etc/skel</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="166"/>
+        <source>lsb-release</source>
+        <translation>lsb-release</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="169"/>
+        <source>Slideshow</source>
+        <translation>Slideshow</translation>
+    </message>
+    <message>
+        <location filename="../res/qml/main.qml" line="172"/>
+        <source>Summary</source>
+        <translation>Resumo</translation>
+    </message>
+</context>
+</TS>
